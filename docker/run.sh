2e946824c89fdbecf7b5d6e1b40d2e4f5e3ef9e2
Christophe Gueret <christophe.gueret@bbc.co.uk>
2016-11-04 12:08:06 +0000

    Added waiting until the schema for spindle is ok

--- docker/run.sh
+++ docker/run.sh
@@ -37,17 +37,25 @@ if [ "${DOINIT}" == "true" ]; then
 		cat /usr/local/src/docker.md
 	fi
 else
-	# Check that the tables exist
+	# Wait until DB Anansi exists
 	until psql --host=postgres --username=postgres -lqt | cut -d \| -f 1 | grep -qw "anansi"; do
-		echo "$(date) - waiting for the table anansi"
+		echo "$(date) - waiting for the DB anansi"
 		sleep 1
 	done
-	until psql --host=postgres --username=postgres -lqt | cut -d \| -f 1 | grep -qw "spindle"; do
-		echo "$(date) - waiting for the table spindle"
-		sleep 1
-	done
+
+	# Wait until DB cluster exists
 	until psql --host=postgres --username=postgres -lqt | cut -d \| -f 1 | grep -qw "cluster"; do
-		echo "$(date) - waiting for the table cluster"
+		echo "$(date) - waiting for the DB cluster"
+		sleep 1
+	done
+
+	# Wait until Spindle exists and is upgraded
+	until psql --host=postgres --username=postgres -lqt | cut -d \| -f 1 | grep -qw "spindle"; do
+		echo "$(date) - waiting for the DB spindle"
+		sleep 1
+	done
+	until psql --host=postgres --username=postgres --dbname=spindle -c "SELECT \"version\" FROM \"_version\" WHERE \"ident\"='com.github.bbcarchdev.spindle.twine';" | grep "24"; do
+		echo "$(date) - waiting for the DB spindle schema version 24"
 		sleep 1
 	done
 fi
