5e5cbe003d502f35dbdff32cb12caa968454def6
Bruce James <bruce.james.ext@bbc.co.uk>
2016-11-11 13:58:20 +0000

    Fix db init

---
+++ docker/init_db.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e
+
+db_host=postgres
+db_user=postgres
+
+# Wait for Postgres
+until nc -z postgres 5432; do
+    echo "$(date) - waiting for Postgres..."
+    sleep 2
+done
+
+for db in anansi spindle cluster; do
+  if psql --host=${db_host} --username=${db_user} --dbname=${db} -tAc '';then
+    echo Database ${db} Exists
+  else
+    echo Creating Database ${db}
+    psql --host=${db_host} --username=${db_user} -c "CREATE DATABASE \"${db}\""
+
+    if [ "$db" == "spindle" ]; then
+      echo Create hstore extension
+      psql --host=${db_host} --username=${db_user} --dbname=${db} -c "CREATE EXTENSION \"hstore\""
+
+      echo "Initialising Twine spindle (via twine)"
+      # Use twine to migrate the schema for 'spindle'
+      twine -d -c /usr/etc/twine.conf -S
+
+      until psql --host=${db_host} --username=${db_user} --dbname=${db} -c "SELECT \"version\" FROM \"_version\" WHERE \"ident\"='com.github.bbcarchdev.spindle.twine';" | grep "24"; do
+        echo "$(date) -waiting for the DB spindle schema version 24"
+        sleep 1
+      done
+    fi
+
+    if [ "$db" == "cluster" ]; then
+      # Starting a writerd will take care of migrating 'cluster'
+      echo "Initialising cluster (via twine-writerd)"
+      twine-writerd
+      until psql --host=${db_host} --username=${db_user} --dbname=${db} -c "SELECT \"version\" FROM \"_version\" WHERE \"ident\"='com.github.bbcarchdev.libcluster';" | grep "5"; do
+        echo "$(date) - waiting for the DB cluster schema version 5"
+        sleep 1
+      done
+      kill -s SIGTERM `pidof twine-writerd`
+    fi
+  fi
+done
+
+exit 0
--- docker/run.sh
+++ docker/run.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 set -e
 
+db_host=postgres
+db_user=postgres
+
 # Wait for Postgres
 until nc -z postgres 5432; do
     echo "$(date) - waiting for Postgres..."
     sleep 2
 done
+
+# Check individual databases are initialised
+for db in anansi spindle cluster; do
+  if psql --host=${db_host} --username=${db_user} --dbname=${db} -tAc '';then
+    echo Database ${db} Exists
+    sleep 5
+  else
+    echo "$(date) - waiting for database ${db} to be initialised"
+    sleep 2
+  fi
+done
+
 
 # Wait for S3
 until nc -z s3 80; do
@@ -19,61 +34,7 @@ until nc -z fourstore 9000; do
     sleep 2
 done
 
-# Wait until someone has initialised everything
-if [ "${DOINIT}" == "true" ]; then
-	if [ ! -f /ready ]; then
-		echo "$(date) - Create the DBs in Postgres..."
-		psql --host=postgres --username=postgres -c "CREATE DATABASE \"anansi\""
-		psql --host=postgres --username=postgres -c "CREATE DATABASE \"spindle\""
-		psql --host=postgres --username=postgres -c "CREATE DATABASE \"cluster\""
-		psql --host=postgres --username=postgres --dbname=spindle -c "CREATE EXTENSION \"hstore\""
-		
-		echo "$(date) -  Initialising Twine..."
-		# Use twine to migrate the schema for 'spindle'
-	    twine -d -c /usr/etc/twine.conf -S    
-
-		# Starting a writerd will take care of migrating 'cluster'
-		twine-writerd
-		until psql --host=postgres --username=postgres --dbname=cluster -c "SELECT \"version\" FROM \"_version\" WHERE \"ident\"='com.github.bbcarchdev.libcluster';" | grep "5"; do
-			echo "$(date) - waiting for the DB cluster schema version 5"
-			sleep 1
-		done
-		kill -s SIGTERM `pidof twine-writerd`
-			    
-		touch /ready
-		
-		# Print some doc
-		cat /usr/local/src/docker.md
-	fi
-else
-	# Wait until DB Anansi exists
-	until psql --host=postgres --username=postgres -lqt | cut -d \| -f 1 | grep -qw "anansi"; do
-		echo "$(date) - waiting for the DB anansi"
-		sleep 1
-	done
-
-	# Wait until Spindle exists and is at latest version
-	until psql --host=postgres --username=postgres -lqt | cut -d \| -f 1 | grep -qw "spindle"; do
-		echo "$(date) - waiting for the DB spindle"
-		sleep 1
-	done
-	until psql --host=postgres --username=postgres --dbname=spindle -c "SELECT \"version\" FROM \"_version\" WHERE \"ident\"='com.github.bbcarchdev.spindle.twine';" | grep "24"; do
-		echo "$(date) - waiting for the DB spindle schema version 24"
-		sleep 1
-	done
-
-	# Wait until DB cluster exists and is at latest version
-	until psql --host=postgres --username=postgres -lqt | cut -d \| -f 1 | grep -qw "cluster"; do
-		echo "$(date) - waiting for the DB cluster"
-		sleep 1
-	done
-	until psql --host=postgres --username=postgres --dbname=cluster -c "SELECT \"version\" FROM \"_version\" WHERE \"ident\"='com.github.bbcarchdev.libcluster';" | grep "5"; do
-		echo "$(date) - waiting for the DB cluster schema version 5"
-		sleep 1
-	done
-fi
-
-echo "$(date) - All good :-)"
+echo "$(date) - Services Ready.."
 
 # Run the requested command
 exec "$@"
