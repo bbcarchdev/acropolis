b809fcbc049a7919339014a78a266f814b07eac1
Bruce James <bruce.james.ext@bbc.co.uk>
2016-11-24 14:49:04 +0000

    Add checkout_develop.sh

---
+++ docker/checkout_develop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd quilt
+git checkout develop
+git pull
+git submodule update --init --recursive
+cd ..
+cd spindle
+git checkout develop
+git pull
+git submodule update --init --recursive
+cd ..
--- test/audiences.py
+++ test/audiences.py
@@ -3,11 +3,12 @@ import urllib.parse;
 
 from rdflib import Namespace
 
+host = "acropolis.localhost"
 rdfs = Namespace("http://www.w3.org/2000/01/rdf-schema#")
 olo = Namespace("http://purl.org/ontology/olo/core#")
 
 
-audiences = [
+audiences_list = [
   'http://bobnational.net/#members',
   'http://shakespeare.acropolis.org.uk/#members',
   'http://bobnational.net/#members,http://shakespeare.acropolis.org.uk/#members',
@@ -15,12 +16,22 @@ audiences = [
   'all',
 ]
 
-for audience in audiences:
+def audiences_query(audiences):
+  query=''
+  for audience in audiences.split(','):
+    if query:
+      query = query + "&"
+    else:
+      query = query + "?"
+    query = query + "for={}".format(urllib.parse.quote(audience))
+  return query
+
+for audiences in audiences_list:
   headers = {
     'Accept': 'application/json',
   }
-  local_uri = 'http://acropolis.localhost/everything?for={}'.format(urllib.parse.quote(audience))
-  uri = 'http://acropolis.localhost/everything?for={}'.format(urllib.parse.quote(audience))
+  local_uri = 'http://{}/assets{}'.format(host, audiences_query(audiences))
+  uri = 'http://{}/assets{}'.format(host, audiences_query(audiences))
   r = requests.get(local_uri, headers=headers)
 
   # r.status_code
@@ -30,8 +41,8 @@ for audience in audiences:
 
   print("-" * 80)
   print(uri)
-  for key in o:
-    print("Key {}".format(key))
+#  for key in o:
+#    print("Key {}".format(key))
   slots = o[uri].get(str(olo.slot),[])
   print("Slots: {}".format(len(slots)))
   for slot in slots:
@@ -39,4 +50,4 @@ for audience in audiences:
     item = slot['http://purl.org/ontology/olo/core#item'][0]['value']
     concept = o[item]
     label = concept['http://www.w3.org/2000/01/rdf-schema#label']
-    print("id: {}, label: {}".format(item, label[0]['value']))
+    #print("id: {}, label: {}".format(item, label[0]['value']))
