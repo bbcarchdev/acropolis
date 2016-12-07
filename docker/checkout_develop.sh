75a4622ca4d61f7c118284a524a20bdd55e1d905
Bruce James <bruce.james.ext@bbc.co.uk>
2016-12-07 10:48:14 +0000

    Loops for checkout

--- docker/checkout_develop.sh
+++ docker/checkout_develop.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-cd quilt
-git checkout develop
-git pull
-git submodule update --init --recursive
-cd ..
-cd spindle
-git checkout develop
-git pull
-git submodule update --init --recursive
-cd ..
-cd twine
-git checkout develop
-git pull
-git submodule update --init --recursive
-cd ..
-cd anansi
-git checkout develop
-git pull
-git submodule update --init --recursive
-cd ..
+for repo in quilt spindle twine anansi; do
+  cd $repo
+  git checkout develop
+  git pull
+  git submodule update --init --recursive
+  cd ..
+done
