3cd5aed6255b07de250ec957ee601a6454001baa
Bruce James <bruce.james.ext@bbc.co.uk>
2017-01-27 09:53:48 +0000

    Tidy scripts

--- vagrant/vagrant_install.sh
+++ vagrant/vagrant_install.sh
@@ -7,21 +7,27 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9E16A4D302DA096A && \
 #echo "deb [arch=amd64] http://apt.bbcarchdev.net/debian/ wheezy main ports live stage dev" \
 #  >> /etc/apt/sources.list.d/bbcarchdev-wheezy.list
 
+apt-get update
+
+#######################################################################
 # Install additional tools for some debugging, running the remote, etc
-apt-get -y update && apt-get install -y --no-install-recommends \
+#######################################################################
+apt-get install -y --no-install-recommends \
   python3 python3-psycopg2 netcat netcat-traditional vim mc ngrep \
   procps tcpdump supervisor postgresql-client git
 
-apt-get -y install libcunit1-ncurses-dev \
+apt-get -y install \
+  libcunit1-ncurses-dev \
   gdb \
-  valgrind
+  valgrind \
+  curl
 
 #################################################
 # Install Apache for running Quilt
 #################################################
 
 # Install Apache
-apt-get -y update && apt-get install -y --no-install-recommends \
+apt-get install -y --no-install-recommends \
   apache2 libapache2-mod-fcgid
 
 export APACHE_RUN_USER=www-data
@@ -38,13 +44,14 @@ a2enmod fcgid
 #################################################
 
  # Install dependencies
- apt-get -y update && apt-get install -y --no-install-recommends \
+ apt-get install -y --no-install-recommends \
    build-essential libtool libltdl-dev automake \
    autoconf pkg-config libcurl4-gnutls-dev autotools-dev \
    libraptor2-dev librasqal3-dev librdf0-dev libfcgi-dev \
    libjansson-dev libxml2-dev libssl-dev \
    flex gettext python-libxml2 libpq-dev libmysqlclient-dev \
-   uuid-dev libncurses5-dev libedit-dev \
-   && rm -rf /var/lib/apt/lists/*
+   uuid-dev libncurses5-dev libedit-dev
+
+ rm -rf /var/lib/apt/lists/*
 
 
